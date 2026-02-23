0515883f4ab568a450bf19693378f959437cad1f
Waled Karim <waledkarim445@gmail.com>
2026-02-23 19:41:11 +0600

    feat: add gradient to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="py-3 border-b border-b-gray-300 mb-10">
       <div className="wrapper">
-        <h1 className="font-logo text-center font-bold">
+        <h1 className="font-logo text-center font-bold bg-linear-to-t from-[#28443F] to-[#F2FD7D] bg-clip-text text-transparent">
           Waled's Frontend Projects
         </h1>
       </div>
